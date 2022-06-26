7c8e6143563dd0493b5bd534e9b5535023b6ad42
Michael Romero <michaelromero068@gmail.com>
2022-06-26 08:46:30 -0400

    First Commit

---
+++ subtraction.py
@@ -0,0 +1,10 @@
+# This program subtracts two numbers
+
+num1 = 500,300
+num2 = 250,500
+
+# Add two numbers
+sum = num1 - num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
